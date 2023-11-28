e0e8c39f48b1cafbf9c9b7d511cf93420242bf4f
adus0205 <adiszu89@gmail.com>
2023-11-28 18:11:04 +0100

    Dodanie edycji statusu zamówienia

---
+++ src/main/java/pl/szupke/onlineShop/admin/order/controller/dto/AdminInitDataDto.java
@@ -0,0 +1,13 @@
+package pl.szupke.onlineShop.admin.order.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class AdminInitDataDto {
+
+    private Map<String, String> orderStatuses;
+}
